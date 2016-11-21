1577ccf35195fcc8bec3bcef86eb13360eb1f950
JustWazzix <justwazzix.pro@gmail.com>
2016-11-21 22:25:37 +0100

    start

---
+++ src/fr/justwazzix/redhub/Core.java
@@ -0,0 +1,17 @@
+package fr.justwazzix.redhub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Core extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+	
+}
