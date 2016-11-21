5902eb8d6379c0e2737ee3fddec6e875e96cc147
JustWazzix <justwazzix.pro@gmail.com>
2016-11-21 23:01:26 +0100

    update

--- src/fr/justwazzix/redhub/login/Login.java
+++ src/fr/justwazzix/redhub/login/Login.java
@@ -3,16 +3,20 @@ package fr.justwazzix.redhub.login;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Login implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String arg, String[] args) {
 		if (sender instanceof Player) {
-		if (LogUtils.isRegister(²)) {
-			
+			Player player = (Player) sender;
+			if (LogUtils.isRegister(player)) {
+				// check login
+			} else {
+				// erreur
+			}
 		}
-	}
 		return false;
 	}
 
